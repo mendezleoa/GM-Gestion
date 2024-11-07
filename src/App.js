405a9392063cbad3aa66b6657ef33aab4bf21a67
mendezleoa <leonardoamm1100@gmail.com>
2024-11-07 16:43:14 -0400

    Creacion de primer componente de presupuesto

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import './App.css';
 import Componente from './componente.js';
 
 function App() {
+  const presupuesto = [{
+    id_: 1,
+    precio: 45,
+    materiales: "Mesa Roble, Pintura Aceite, Otros",
+    manoObra: 23,
+    ganancia: 20
+  }, {
+    id_: 2,
+    precio: 40,
+    materiales: "Mesa Blanca, Pintura Aerosol, Otros",
+    manoObra: 20,
+    ganancia: 20
+  }]
   return (
     <div className="App">
-      <header className="App-header">
-        <div className='border-4 border-green-500/100'>
-          <Componente numero={1} />
-          <Componente numero={2} />
+      <header className="App-header bg-slate-800 text-slate-300">
+        <div className='m-2'>
+          {presupuesto.map((datospres) => (
+            <Componente key={datospres.id_} presupuesto={datospres} />
+          ))}
         </div>
       </header>
     </div>
--- src/componente.js
+++ src/componente.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Componente(props) {
-    const [numero, setNumero] = useState(props.numero);
-    return(
-        <div>
-            <p>
-            {numero}
+    const presupuesto = (props.presupuesto);
+    return (
+        <div className='border-4 border-green-500/100 m-5 p-3 bg-slate-300 text-slate-700'>
+            <p className='text-4xl py-2'>
+                Total: {(presupuesto.precio + presupuesto.manoObra) * ((presupuesto.ganancia / 100) + 1)} $
             </p>
-            <button className='bg-green-400' onClick={() => {
-                if (numero === 9) {
-                    setNumero(0)
-                } else {
-                    setNumero(numero + 1)
+            <p className='text-2xl py-2'>
+                {presupuesto.materiales}
+            </p>
+            <button
+                className='bg-green-500/100 rounded-xl px-5 py-2 border-2 text-slate-900 border-slate-500 hover:bg-green-200/100 hover:border-green-500/100 hover:text-slate-800'
+                onClick={() =>
+                    console.log("Mensaje enviado", presupuesto.id_)
                 }
-            }}>Boton</button>
+            >
+                Boton
+            </button>
         </div>
     )
 }
